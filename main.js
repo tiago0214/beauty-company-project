15684954ac51b0669daf9650918ffaa30259701b
Tiago Souza <tiago0214@gmail.com>
2024-04-24 23:50:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById('menu');
+const listaItens = document.getElementById('lista');
+
+menu.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (listaItens.style.display === "block") {
+        listaItens.style.display = 'none';
+    } else {
+        listaItens.style.display = "block";
+    }
+})

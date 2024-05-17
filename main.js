06d9b9db3cc9c01540a46be123b66bc2c4786175
Tiago Souza <tiago0214@gmail.com>
2024-05-17 09:40:53 +0100

    att

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menu = document.getElementById('menu');
 const listaItens = document.getElementById('lista');
+const sellProducts = document.querySelector('.container__sell--products');
+let itens = []
 
 menu.addEventListener('click', (event) => {
     event.preventDefault();
@@ -9,3 +11,51 @@ menu.addEventListener('click', (event) => {
         listaItens.style.display = "block";
     }
 })
+
+function createElement(product) {
+    const ul = document.createElement('ul');
+    ul.classList.add('container__sell--card');
+
+    const imgParent = document.createElement('li');
+    imgParent.classList.add('container__sell--card--img');
+
+    const img = document.createElement('img');
+    img.classList.add('container__sell--card--img');
+    img.setAttribute('src', product.img);
+
+    const name = document.createElement('li');
+    name.classList.add('container__sell--card--name');
+    name.innerHTML = `${product.name}`
+
+    const value = document.createElement('li');
+    value.classList.add('container__sell--card--value');
+    value.innerHTML = `${product.value}`
+
+    const linkParent = document.createElement('li');
+    linkParent.classList.add('container__sell--card--link');
+
+    const link = document.createElement('a');
+    link.setAttribute('href', '#')
+    link.classList.add('container__sell--card--item--link');
+    link.innerHTML = `ADD TO BAG`
+
+    ul.appendChild(imgParent);
+    ul.appendChild(name);
+    ul.appendChild(value);
+    ul.appendChild(linkParent);
+
+    imgParent.appendChild(img);
+    linkParent.appendChild(link);
+
+    return ul
+}
+
+fetch('./products/products.json')
+    .then((response) => response.json())
+    .then((json) => {
+        itens = json
+        sellProducts.innerHTML = ""
+        itens.forEach(element => {
+            sellProducts.appendChild(createElement(element))
+        });
+    })

0ed886f5b41cb3604bc1654ea4c75bfbdd92d59d
Tiago Souza <tiago0214@gmail.com>
2024-07-11 13:32:44 +0100

    change route /

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,10 +2,17 @@ import express from 'express';
 import mascarasRoutes from './mascarasProducts.js';
 import trendingsRoutes from './trendingsProducts.js'
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+const pathFile = `${__dirname}/public`
+
+console.log(pathFile)
+
 export function routes (app){
-  app.get(('/'), (req, res) => {
-    return res.status(200).send("Welcome: this is a back-end server for a project on boticário.")
-  });
+  app.use('/', express.static(pathFile))
 
   app.use(express.json(),mascarasRoutes,trendingsRoutes);
 }
